4c75b2e91a3c3e68fb4c44f788def51840cdd790
6666 <1040739593@qq.com>
2018-12-28 09:06:28 +0800

    添加java327工程到git

---
+++ java327/src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+        System.out.println("ccc");
+        System.out.println("ddd");
+    }
+}
